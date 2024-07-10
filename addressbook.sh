e9c1bc7dceaf57de28e1aa383b0c8960ba4cdbdb
Clement Wu <inchiawu@gmail.com>
2024-07-10 16:58:34 -0700

    still doesn't work

--- addressbook.sh
+++ addressbook.sh
@@ -5,32 +5,40 @@ printf "3. Edit\n"
 printf "4. Remove\n"
 printf "5. Quit\n"
 
-#Lab 5: 1) Already in case form
-
 while :
 do
 	printf "\nEnter your selection: "
 	read VAR_INPUT
 	case $VAR_INPUT in 
 		1) echo -n "Searching:"
-		#!/bin/bash
-		INPUT=contacts.csv		
-		OLDIFS=$IFS
+		
+		CSV_FILE="/home/clement/Downloads/contacts.csv"
+
+		if [ ! -f "$CSV_FILE" ]; then
+			echo "Error: CSV File '$CSV_FILE' not found"
+			exit 1
+		fi
+		
+		#delimiter: .
 		IFS=','
-		[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
-		while read id fname lname company address city county state acode pnumber email
-			printf "Name: %-8fname %-8lname Company: %-10company City: %-10city 
-				County: %-10county State: %-2state Area Code: %-5acode 
-				Phone Number: %-13pnumber Email: %-10email 
-		done < $INPUT
-		IFS=$OLDIFS
-		2) echo -n "Developing: Feature Not Required in Lab 5"
+		
+		while read id fname lname company address city county state acode pnumber email 
+		do
+			printf "ID: %-5s Name: %-20s %-20s Company: %-20s\n" "$id" "$fname" "$lname" "$company"
+  			printf "Address: %-40s\n" "$address"
+  			printf "City: %-20s County: %-20s State: %-2s Area Code: %-5s\n" "$city" "$county" "$state" "$acode"
+  			printf "Phone Number: %-13s Email: %-20s\n\n" "$pnumber" "$email" 
+		done < "$CSV_FILE"
+		IFS=$'\n'
+
+		echo "Process Finished."
+		2) echo -n "In Development: Feature Not Required in Lab 5"
 		   ;;
-		3) echo -n "Developing: Feature Not Required in Lab 5"
+		3) echo -n "In Development: Feature Not Required in Lab 5"
 		   ;;
-		4) echo -n "Developing: Feature Not Required in Lab 5"
+		4) echo -n "In Development: Feature Not Required in Lab 5"
 		   ;;
-		5) echo -n  "Exit Program"
+		5) echo -n "Exit Program"
 		sleep 1
 		break
 		   ;;
