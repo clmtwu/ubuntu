cb41925637de6e09d77d3b93fc03c144719bc8c1
Clement Wu <inchiawu@gmail.com>
2024-08-02 10:58:00 -0700

    what am i doing

--- addressbook.sh
+++ addressbook.sh
@@ -4,7 +4,7 @@ do_list() {
         if [ ! -f "$CONTACT" ]; then
         	echo "Error: CSV File '$CONTACT' not found"
                 sleep 1
-                break
+                exit 1
         fi
         #delimiter: ,
         IFS=','
@@ -20,16 +20,14 @@ do_list() {
 }
 
 do_search() {
-}
-
-do_add() {
-}
-
-do_edit() {
+	echo "if i need to implement binary search im going to slit my wrist"
 }
 
 do_remove() {
+	echo -n "Removing File..."
+        rm -f "/home/clement/Downloads/contacts.csv" 
 }
+
 
 printf "%s" "-- Address Book --"
 printf "\n1. List / Search\n"
@@ -43,15 +41,22 @@ do
 	printf "\nEnter your selection: "
 	read VAR_INPUT
 	case $VAR_INPUT in 
-		1) do_list 
+		1) printf "\nWould you like to list  or search?"
+		   printf "1. List\n"
+		   printf "2. Search\n"
+		   read ES_INPUT
+		   case $ES_INPUT in
+			1) do_list
+		   	   ;;
+			2) do_search
+			   ;; 
 		   ;;
 		2) echo -n "In Development: Feature Not Required in Lab 5"
 		   ;;
 		3) echo -n "In  Development: Feature Not Required in Lab 5"
 		   ;;
-		4) echo -n "Removing File..."
-		   	rm -f "/home/clement/Downloads/contacts.csv" 
-		   	;;
+		4) do_remove
+		   ;;
 		5) echo -n "Exiting Program..."
 		sleep 1
 		break
