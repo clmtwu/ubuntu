c206095bf88bd9ccdc0aca9093deccd0324fed1e
Clement Wu <inchiawu@gmail.com>
2024-08-02 12:14:37 -0700

    update

--- addressbook.sh
+++ addressbook.sh
@@ -8,19 +8,32 @@ do_list() {
         fi
         #delimiter: ,
         IFS=','
-        while read id fname lname address city county state acode pnumber email 
+        while read id firstName lastName address city county state zip  phone email 
         do
-                printf "ID: %-2s Name: %-10s %-10s" "$id" "$fname" "$lname"
+                printf "ID: %-2s Name: %-10s %-10s" "$id" "$firstName" "$lastName"
                 printf "Address: %-20s\n" "$address"
-                printf "City: %-10s County: %-10s State: %-2s Area Code: %-5s\n" "$city" "$county" "$state" "$acode"
-                printf "Phone Number: %-13s Email: %-15s\n\n" "$pnumber" "$email" 
+                printf "City: %-10s County: %-10s State: %-2s Area Code: %-5s\n" "$city" "$county" "$state" "$zip"
+                printf "Phone Number: %-13s Email: %-15s\n\n" "$phone" "$email" 
 	done < "$CONTACT"
         IFS=$'\n'
 	echo "Process Finished."
 }
 
 do_search() {
-	echo "if i need to implement binary search im going to slit my wrist"
+	# first name is always after id, so we traverse the csv until the first name and backtrack to id
+	CONTACT="/home/clement/Downloads/contacts.csv"
+	printf "What is the first name of the person you are searching for?\n"
+	found=0
+	read firstNameInput
+	awk -F',' -v search="$firstNameInput" '$1 == search {print; found=1; exit}'$CONTACT
+	if [ $found = 0 ]; then
+		echo "Error: Name not found"
+	fi
+}
+
+do_add(){
+	# no duplicate checking implementation since we could have two identical entries separated by id #
+	do_search
 }
 
 do_remove() {
@@ -41,15 +54,15 @@ do
 	printf "\nEnter your selection: "
 	read VAR_INPUT
 	case $VAR_INPUT in 
-		1) printf "\nWould you like to list  or search?"
+		1) printf "\nWould you like to list  or search?\n"
 		   printf "1. List\n"
 		   printf "2. Search\n"
 		   read ES_INPUT
-		   case $ES_INPUT in
-			1) do_list
-		   	   ;;
-			2) do_search
-			   ;; 
+		   if [ "$ES_INPUT" == "1" ]; then
+			do_list
+		   else
+			do_search
+		   fi 
 		   ;;
 		2) echo -n "In Development: Feature Not Required in Lab 5"
 		   ;;
