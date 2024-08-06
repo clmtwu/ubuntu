d832367f30186b622ab2309cc4f2b402f8593dca
Clement Wu <inchiawu@gmail.com>
2024-08-06 13:44:38 -0700

    twin

--- shell.sh
+++ shell.sh
@@ -1,1 +1,12 @@
-$ echo $0
+#!/bin/bash
+
+# Get the PPID of the current process
+parent_pid=$$
+
+# Use ps to get the command line of the parent process
+parent_cmd=$(ps -p $parent_pid -o cmd=)
+
+# Extract the shell name from the command line
+shell_name=$(echo $parent_cmd | cut -d' ' -f1)
+
+echo "Executing shell: $shell_name"
