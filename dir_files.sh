6aedaf90da5fdd8c52109458c596961996accefb
Clement Wu <inchiawu@gmail.com>
2024-08-06 14:03:44 -0700

    Question 4

---
+++ dir_files.sh
@@ -0,0 +1,6 @@
+# Write a shell script that...
+for file in *; do           # "all... files... in the working directory"
+  if [ -d "$file" ]; then   # "directory files, but no other types of files..."
+    echo "$file"            # "displays the names..."
+  fi
+done
