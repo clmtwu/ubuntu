c0b2a9f94bac58e18d60dbe291a50e2047a24db7
Clement Wu <inchiawu@gmail.com>
2024-08-06 13:51:21 -0700

    please still work

--- traverse.sh
+++ traverse.sh
@@ -1,6 +1,7 @@
 
 doindent() {
   # Do a small indent depending on how deep into the tree we are
+  # using echo "\c" instead of echo en " " since I am using ubuntu
   for i in $(seq 1 "$1"); do
     echo "\c "
   done
@@ -8,7 +9,7 @@ doindent() {
 
 traverse() {
   # Traverse a directory
-  local indent="$2"  # Use "local" to keep variable within function
+  indent="$2" 
 
   # Loop through each item in the directory
   for item in "$1"/*; do
@@ -23,7 +24,7 @@ traverse() {
   done
 }
 
-# Check if argument provided, otherwise use current directory
+# NOTE: UNCHANGED AS GIVEN FROM PROMPT
 if [ -z "$1" ]; then
   traverse . 0
 else
